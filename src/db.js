cdc95b6d0c9bd9ecc86e09f9a46c15d506d9de11
kitty-kad <>
2021-12-15 19:49:02 +0000

    Use cloud db url

--- src/db.js
+++ src/db.js
@@ -1,12 +1,21 @@
 const { Pool } = require('pg');
-const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+let pool;
+if (process.env.IS_DEV) {
+    pool = new Pool({
+            user: process.env.DB_USER,
+            host: process.env.DB_HOST,
+            database: process.env.DB_NAME,
+            password: process.env.DB_PASSWORD,
+            port: process.env.DB_PORT,
+    });
+} else {
+    pool = new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false
+        }
+      });
 }
-);
 
 const THIRTY_DAYS_SECONDS = 60*60*24*30;
 
