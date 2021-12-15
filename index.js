e99b6f60e4054989bea49fb5149bfc3037ff3317
kitty-kad <>
2021-12-15 17:53:37 +0000

    Used more generic naming for port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
-const port = process.env.APP_PORT;
+const port = process.env.PORT;
 const {getTokenPrices} = require('./src/db');
 
 
