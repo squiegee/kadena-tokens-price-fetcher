955831c4a1eb746f7bc15a35c88a46a2d735cccf
kitty-kad <>
2022-01-16 11:09:49 +0000

    Return more results

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
-require('dotenv').config()
-const express = require('express');
-const cors = require('cors');
-const {getTokenPrices} = require('./src/db');
-const tokensData = require('./src/tokens.json');
-
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { getTokenPrices } = require("./src/db");
+const tokensData = require("./src/tokens.json");
 
 const app = express();
 
 app.use(cors());
-app.options('*', cors());
+app.options("*", cors());
 
-app.get('/getPrices', async (req, res) => {
-    const {tokenAddress, fromTime, toTime} = req.query;
-    const prices = await getTokenPrices(tokenAddress, fromTime, toTime);
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.json(prices);
+app.get("/getPrices", async (req, res) => {
+  const { tokenAddress, fromTime, toTime } = req.query;
+  let prices = [];
+  try {
+    prices = await getTokenPrices(tokenAddress, fromTime, toTime);
+  } catch (e) {
+    console.log(e);
+  }
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.json(prices);
 });
 
-app.get('/isRunning', async (_req, res) => {
-    res.send('running');
+app.get("/isRunning", async (_req, res) => {
+  res.send("running");
 });
 
-app.get('/getTokenMetadata', async (_req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.json(tokensData);
+app.get("/getTokenMetadata", async (_req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.json(tokensData);
 });
 
 const port = process.env.PORT;
 app.listen(port, () => {
-    console.log(`Listening on http://localhost:${port}`)
+  console.log(`Listening on http://localhost:${port}`);
 });
--- src/db.js
+++ src/db.js
@@ -1,71 +1,76 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 let pool;
 if (process.env.IS_DEV) {
-    pool = new Pool({
-            user: process.env.DB_USER,
-            host: process.env.DB_HOST,
-            database: process.env.DB_NAME,
-            password: process.env.DB_PASSWORD,
-            port: process.env.DB_PORT,
-    });
+  pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  });
 } else {
-    pool = new Pool({
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-          rejectUnauthorized: false
-        }
-      });
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
 }
 
-const THIRTY_DAYS_SECONDS = 60*60*24*30;
+const THIRTY_DAYS_SECONDS = 60 * 60 * 24 * 30;
 
 async function getTokenPrices(tokenAddress, fromTime, toTime) {
-    if (tokenAddress == null || tokenAddress === ''  ){
-        return [];
-    }
-    const currTime = Math.floor(new Date().getTime() / 1000);
-    fromTime = fromTime ?? currTime - THIRTY_DAYS_SECONDS;
-    toTime = toTime ?? currTime;
-    const params = [ tokenAddress, fromTime,toTime];
-    const countQuery = `SELECT count(*) FROM token_prices WHERE token_address = $1
-    AND unix_time >= $2 AND unix_time <= $3;`
-    const counts = await executeQuery(countQuery, params);
-    const selectWhere = `WHERE token_address = $1
-    AND unix_time >= $2 AND unix_time <= $3` 
-    let selectQuery = `SELECT * FROM token_prices ${selectWhere};`;
-    // If we have too many results, lets reduce how many rows we return   
-    if (counts[0].count >= 150) {
-        const modulo = Math.ceil(counts[0].count / 100);
-        selectQuery = `
+  if (tokenAddress == null || tokenAddress === "") {
+    return [];
+  }
+  const currTime = Math.floor(new Date().getTime() / 1000);
+  fromTime = fromTime ?? currTime - THIRTY_DAYS_SECONDS;
+  toTime = toTime ?? currTime;
+  const params = [tokenAddress, fromTime, toTime];
+  const countQuery = `SELECT count(*) FROM token_prices WHERE token_address = $1
+    AND unix_time >= $2 AND unix_time <= $3;`;
+  const counts = await executeQuery(countQuery, params);
+  const selectWhere = `WHERE token_address = $1
+    AND unix_time >= $2 AND unix_time <= $3`;
+  let selectQuery = `SELECT * FROM token_prices ${selectWhere};`;
+  // If we have too many results, lets reduce how many rows we return
+  if (counts[0].count >= 250) {
+    const modulo = Math.ceil(counts[0].count / 100);
+    selectQuery = `
             SELECT t.*
             FROM (
                 SELECT *, row_number() OVER(ORDER BY unix_time ASC) AS row
                     FROM token_prices ${selectWhere}
                 ) t
             WHERE t.row % ${modulo} = 0;
-        `
-    }
-    const results = await executeQuery(selectQuery, params);
-    return results;
+        `;
+  }
+  const results = await executeQuery(selectQuery, params);
+  return results;
 }
 
 async function insertTokenPrices(pricesData) {
-    if (pricesData.length === 0 ){
-        return;
-    }
-    await createTablesIfNotExists();
-    const unixTime = Math.floor(new Date().getTime() / 1000);
-    for (const data of pricesData) {
-        const query = `INSERT INTO token_prices
+  if (pricesData.length === 0) {
+    return;
+  }
+  await createTablesIfNotExists();
+  const unixTime = Math.floor(new Date().getTime() / 1000);
+  for (const data of pricesData) {
+    const query = `INSERT INTO token_prices
         (unix_time,token_address,price_in_usd,price_in_kda)
-        VALUES ($1,$2,$3,$4);`
-        const params =[unixTime, data.tokenAddress, data.priceInUsd, data.priceInKda];
-        await executeQuery(query, params);
-    }
+        VALUES ($1,$2,$3,$4);`;
+    const params = [
+      unixTime,
+      data.tokenAddress,
+      data.priceInUsd,
+      data.priceInKda,
+    ];
+    await executeQuery(query, params);
+  }
 }
 
 async function createTablesIfNotExists() {
-    const query = `
+  const query = `
     CREATE TABLE IF NOT EXISTS "token_prices" (
         "unix_time" BIGINT,
 	    "token_address" VARCHAR(32) NOT NULL,
@@ -73,19 +78,32 @@ async function createTablesIfNotExists() {
         "price_in_kda" FLOAT
     );
     CREATE INDEX IF NOT EXISTS token_address_time ON token_prices (token_address, unix_time);
-    `
-    executeQuery(query);
+    CREATE TABLE IF NOT EXISTS "token_prices_timed" (
+        "time_type" VARCHAR(16),
+        "unix_time" BIGINT,
+	    "token_address" VARCHAR(32) NOT NULL,
+	    "price_in_usd_low" FLOAT,
+        "price_in_usd_high" FLOAT,
+        "price_in_usd_start" FLOAT,
+        "price_in_usd_end" FLOAT,
+        "price_in_kda_low" FLOAT,
+        "price_in_kda_high" FLOAT,
+        "price_in_kda_start" FLOAT,
+        "price_in_kda_end" FLOAT
+    );
+    CREATE INDEX IF NOT EXISTS token_address_time ON token_prices_timed (token_address, time_type, unix_time);
+    `;
+  executeQuery(query);
 }
 
-async function executeQuery (query, params=[]) {
-    try {
-        const res = await pool.query(query, params);
-        return res.rows;
-    } catch (error) {
-        console.error(error.stack);
-        return [];
-    }
-};
+async function executeQuery(query, params = []) {
+  try {
+    const res = await pool.query(query, params);
+    return res.rows;
+  } catch (error) {
+    console.error(error.stack);
+    return [];
+  }
+}
 
-module.exports = {getTokenPrices, insertTokenPrices}
-
+module.exports = { getTokenPrices, insertTokenPrices };
