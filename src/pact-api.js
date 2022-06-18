488953a19e95b98291675bd5e8e48ece3b832bb5
kitty-kad <>
2022-06-18 16:33:07 +0100

    Added kdswap

--- src/pact-api.js
+++ src/pact-api.js
@@ -1,16 +1,15 @@
-const Pact = require('pact-lang-api');
+const Pact = require("pact-lang-api");
 
-const chainId = "3";
-const network = `https://api.chainweb.com/chainweb/0.0/mainnet01/chain/${chainId}/pact`;
-
-const creationTime = () => Math.round((new Date).getTime()/1000)-10;
+const creationTime = () => Math.round(new Date().getTime() / 1000) - 10;
 const GAS_PRICE = 0.00000001;
 
-const getTokenToKadena = async (token) => {
-    try {
-      let data = await Pact.fetch.local({
-          pactCode: `
-          (use free.exchange)
+const getTokenToKadena = async (token, exchange, chainId) => {
+  const network = `https://api.chainweb.com/chainweb/0.0/mainnet01/chain/${chainId}/pact`;
+  try {
+    let data = await Pact.fetch.local(
+      {
+        pactCode: `
+          (use ${exchange})
           (let*
             (
               (p (get-pair ${token} coin))
@@ -18,22 +17,34 @@ const getTokenToKadena = async (token) => {
               (reserveB (reserve-for p coin))
             )[reserveA reserveB])
            `,
-           meta: Pact.lang.mkMeta("account", chainId ,GAS_PRICE,3000,creationTime(), 600),
-        }, network);
-        if (data.result.status === "success"){
-            const tokenReserve = getReserve(data.result.data[0]);
-            const kadenaReserve = getReserve(data.result.data[1]);
-            const ratio = kadenaReserve/ tokenReserve;
-            return ratio;
-        }
-    } catch (e) {
-      console.log(e)
+        meta: Pact.lang.mkMeta(
+          "account",
+          chainId,
+          GAS_PRICE,
+          3000,
+          creationTime(),
+          600
+        ),
+      },
+      network
+    );
+    if (data.result.status === "success") {
+      const tokenReserve = getReserve(data.result.data[0]);
+      const kadenaReserve = getReserve(data.result.data[1]);
+      const ratio = kadenaReserve / tokenReserve;
+      return ratio;
+    } else {
+      console.log(data.result);
     }
-    return null;
+  } catch (e) {
+    console.log("NULLY2");
+    console.log(e);
   }
+  return null;
+};
 
 const getReserve = (tokenData) => {
-    return tokenData.decimal? tokenData.decimal:  tokenData;
-}
+  return tokenData.decimal ? tokenData.decimal : tokenData;
+};
 
-module.exports = {getTokenToKadena};
+module.exports = { getTokenToKadena };
--- src/price-fetcher.js
+++ src/price-fetcher.js
@@ -1,56 +1,62 @@
-const fss = require('fs');
+const fss = require("fs");
 const fs = fss.promises;
-const {getTokenToKadena} = require('./pact-api');
-const CoinGecko = require('coingecko-api/lib/CoinGecko');
-const tokensData = require('./tokens.json');
+const { getTokenToKadena } = require("./pact-api");
+const CoinGecko = require("coingecko-api/lib/CoinGecko");
+const tokensData = require("./tokens.json");
 
-async function fetchAllPrices () {
-    // 1. Get the current KDA price
-    const CoinGeckoClient = new CoinGecko();
-    const coinGeckoResponse = await retryNetworkRequest(async () => CoinGeckoClient.simple.price({ids: "kadena", vs_currencies: "usd"}),
-        'get KDA price in USD from coin gecko');
-    const kdaToUsd = coinGeckoResponse.data.kadena.usd;
-    if (kdaToUsd == null) {
-        throw new Error ('COULD NOT GET KDA PRICE IN USD');
+async function fetchAllPrices() {
+  // 1. Get the current KDA price
+  const CoinGeckoClient = new CoinGecko();
+  const coinGeckoResponse = await retryNetworkRequest(
+    async () =>
+      CoinGeckoClient.simple.price({ ids: "kadena", vs_currencies: "usd" }),
+    "get KDA price in USD from coin gecko"
+  );
+  const kdaToUsd = coinGeckoResponse.data.kadena.usd;
+  if (kdaToUsd == null) {
+    throw new Error("COULD NOT GET KDA PRICE IN USD");
+  }
+  // 2. Go through each token, get the price in KDA and use KDA -> USD calculate USD value
+  const prices = [];
+  for (const token of tokensData) {
+    try {
+      const priceInKda = await retryNetworkRequest(
+        async () =>
+          getTokenToKadena(token.address, token.exchange, token.chainId),
+        `fetch ${token.name} in KDA`
+      );
+      if (priceInKda == null) {
+        return;
+      }
+      const priceInUsd = priceInKda * kdaToUsd;
+      prices.push({ tokenAddress: token.address, priceInUsd, priceInKda });
+    } catch (e) {
+      console.log(`Could not fetch token ${token.address}, got exception ${e}`);
     }
-    // 2. Go through each token, get the price in KDA and use KDA -> USD calculate USD value
-    const prices = [];
-    for(const token of tokensData) {
-        try {
-        const priceInKda = await retryNetworkRequest(async()=>getTokenToKadena(token.address), 
-            `fetch ${token.name} in KDA`);
-        if (priceInKda == null) {
-            return;
-        }
-        const priceInUsd = priceInKda*kdaToUsd;
-        prices.push({tokenAddress: token.address, priceInUsd, priceInKda});
-        } catch (e) {
-            console.log(`Could not fetch token ${token}, got exception ${e}`);
-        }
-    }
-    return prices;
+  }
+  return prices;
 }
 
-async function retryNetworkRequest (f, name) {
-    for (let i =0; i <3; i++) {
-        try {
-            const response = await f();
-            if (response != null) {
-                return response;
-            }
-        } catch (e){
-            console.log(`Failed request ${name} with error ${e}`);
-            // do nothing and try again
-        }
-        await sleep(1000);
+async function retryNetworkRequest(f, name) {
+  for (let i = 0; i < 3; i++) {
+    try {
+      const response = await f();
+      if (response != null) {
+        return response;
+      }
+    } catch (e) {
+      console.log(`Failed request ${name} with error ${e}`);
+      // do nothing and try again
     }
-    console.log(`Network request ${name} failed after 3 attempts`);
+    await sleep(1000);
+  }
+  console.log(`Network request ${name} failed after 3 attempts`);
 }
 
 async function sleep(ms) {
-    return new Promise((resolve) => {
-        setTimeout(resolve, ms);
-    }).promise();
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  }).promise();
 }
 
-module.exports = {fetchAllPrices};
+module.exports = { fetchAllPrices };
