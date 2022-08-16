ef4e9f46eb9556915a30c14492a929f8820332e4
Squiegee <97313350+squiegee@users.noreply.github.com>
2022-08-16 09:38:52 -0500

    Adds getFactoryTokenToKadena()
    + getFactoryTokenToKadena() 
       function adds poolid parameter which is used to get stakefactory pool prices

--- src/pact-api.js
+++ src/pact-api.js
@@ -43,6 +43,46 @@ const getTokenToKadena = async (token, exchange, chainId) => {
   return null;
 };
 
+const getFactoryTokenToKadena = async (poolid, token, exchange, chainId) => {
+  const network = `https://api.chainweb.com/chainweb/0.0/mainnet01/chain/${chainId}/pact`;
+  try {
+    let data = await Pact.fetch.local(
+      {
+        pactCode: `
+          (use ${exchange})
+          (let*
+            (
+              (p (get-pair ${token} coin ${poolid}))
+              (reserveA (reserve-for p ${token}))
+              (reserveB (reserve-for p coin))
+            )[reserveA reserveB])
+           `,
+        meta: Pact.lang.mkMeta(
+          "account",
+          chainId,
+          GAS_PRICE,
+          3000,
+          creationTime(),
+          600
+        ),
+      },
+      network
+    );
+    if (data.result.status === "success") {
+      const tokenReserve = getReserve(data.result.data[0]);
+      const kadenaReserve = getReserve(data.result.data[1]);
+      const ratio = kadenaReserve / tokenReserve;
+      return ratio;
+    } else {
+      console.log(data.result);
+    }
+  } catch (e) {
+    console.log("NULLY2");
+    console.log(e);
+  }
+  return null;
+};
+
 const getReserve = (tokenData) => {
   return tokenData.decimal ? tokenData.decimal : tokenData;
 };
